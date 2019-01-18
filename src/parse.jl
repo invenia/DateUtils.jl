ae92554b8311c413eb0c621f22715afba784bf73
rofinn <rory.finnegan@gmail.com>
2019-01-18 17:39:33 -0600

    Initial work getting DateTime, ZonedDateTime and Interval parsing working.

--- src/parse.jl
+++ src/parse.jl
@@ -27,3 +27,102 @@ function Base.parse(::Type{Period}, str::AbstractString)
         throw(ArgumentError("Cannot parse period of: $str"))
     end
 end
+
+#=
+Code for parsing intervals with a focus on testing DateTimes and ZonedDateTimes
+
+In order to guarantee that datetime parsing will work properly you'll need to call
+```
+Dates.format(dt, Dates.ISODateTimeFormat)
+```
+and
+```
+Dates.format(zdt, TimeZones.ISOZonedDateTimeFormat)
+```
+when generating the strings.
+
+NOTE: We can currently handle when milliseconds are missing.
+=#
+
+function Base.parse(::Type{Interval{T}}, str::AbstractString) where T
+    #=
+    print(
+        io,
+        first(inclusivity(interval)) ? "[" : "(",
+        first(interval),
+        " .. ",
+        last(interval),
+        last(inclusivity(interval)) ? "]" : ")",
+    )
+    =#
+    a, b = parse_inclusivity(str)
+    f, l = _parse.(T, strip.(split(str[2:end-1], "..")))
+
+    return Interval(f, l, a, b)
+end
+
+function Base.parse(::Type{<:AnchoredInterval{P, <:AbstractDateTime}}, str::AbstractString) where P
+    # (2018-12-02 HE24-06:00]
+    a, b = parse_inclusivity(str)
+    dt, suffix = split(str[2:end-1], ' ')
+
+    f = if startswith(suffix, "HE")
+        HE
+    elseif startswith(suffix, "HB")
+        HB
+    else
+        throw(ArgumentError("Can only parse HE and HB anchored intervals"))
+    end
+
+    hr = parse(Int, suffix[3:4])
+
+    anchor = if length(suffix) > 4
+        ZonedDateTime(parse.(Int, split(dt, '-'))..., hr, FixedTimeZone(suffix[5:end]))
+    else
+        DateTime(parse.(Int, split(dt, '-'))..., hr)
+    end
+
+    return f(anchor, a, b)
+end
+
+_parse(::Type{T}, str::AbstractString) where T = parse(T, str)
+
+function _parse(::Type{T}, str::AbstractString) where T <: DateTime
+    if '.' in str
+        parse(T, str)
+    else
+        parse(T, "$str.0")
+    end
+end
+
+function _parse(::Type{T}, str::AbstractString) where T <: ZonedDateTime
+    if '.' in str
+        parse(T, str)
+    else
+        dt, t = split(str, 'T')
+        tz_char = '+' in t ? '+' : '-'
+        s = string(dt, 'T', join(split(t, tz_char), ".000$tz_char"))
+        # println(s)
+        parse(T, s)
+    end
+end
+
+function parse_inclusivity(str::AbstractString)
+    a = if str[1] == '['
+        true
+    elseif str[1] == '('
+        false
+    else
+        throw(ArgumentError("Interval strings must start with '[' or '('."))
+    end
+
+    b = if str[end] == ']'
+        true
+    elseif str[end] == ')'
+        false
+    else
+        throw(ArgumentError("Interval strings must end with ']' or ')'."))
+    end
+
+    return a, b
+end
--- test/parse.jl
+++ test/parse.jl
@@ -1,4 +1,6 @@
 using Compat.Dates
+using Intervals
+using TimeZones
 
 @testset "parse Period" begin
     @test parse(Period, "Year(1)") == Year(1)
@@ -8,5 +10,41 @@ using Compat.Dates
 
     for (name, T) in DateUtils.STRING_TO_PERIOD
         @test parse(Period, "$name(0)") == T(0)
+    end
+end
+
+@testset "parse datetimes" begin
+    a = DateTime(2019, 1, 18, 16)
+    b = DateTime(2019, 1, 18, 16, 46, 12, 285)
+    x = ZonedDateTime(a, tz"America/Winnipeg")
+    y = ZonedDateTime(b, tz"America/Winnipeg")
+
+    @testset "_parse DateTime" begin
+        @test DateUtils._parse(DateTime, string(a)) == parse(DateTime, Dates.format(a, Dates.ISODateTimeFormat))
+        @test DateUtils._parse(DateTime, string(b)) == parse(DateTime, Dates.format(b, Dates.ISODateTimeFormat))
+        @test DateUtils._parse(DateTime, string(b)) == parse(DateTime, string(b))
+        @test DateUtils._parse(DateTime, string(a)) == parse(DateTime, string(a))     # Does this error?
+    end
+
+    @testset "_parse ZonedDateTime" begin
+        @test DateUtils._parse(ZonedDateTime, string(x)) == parse(ZonedDateTime, Dates.format(x, TimeZones.ISOZonedDateTimeFormat))
+        @test DateUtils._parse(ZonedDateTime, string(y)) == parse(ZonedDateTime, Dates.format(y, TimeZones.ISOZonedDateTimeFormat))
+        @test DateUtils._parse(ZonedDateTime, string(y)) == parse(ZonedDateTime, string(y))
+        @test DateUtils._parse(ZonedDateTime, string(x)) == x
+        @test_throws ArgumentError parse(ZonedDateTime, string(x))
+    end
+
+    anchored_intervals = [HE.((a, b, x, y))..., HB.((a, b, x, y))...]
+    @testset "parse AnchoredInterval" begin
+        for i in anchored_intervals
+            @test parse(typeof(i), string(i)) == i
+        end
+    end
+
+    intervals = [Interval(x, x + Hour(1)) for x in (a, b, x, y)]
+    @testset "parse Interval" begin
+        for i in intervals
+            @test parse(typeof(i), string(i)) == i
+        end
     end
 end
