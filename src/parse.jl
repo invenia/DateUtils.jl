69bcf183425a3a15c5f702a6f08742638863bacd
rofinn <rory.finnegan@gmail.com>
2019-01-19 16:46:37 -0600

    DateTime parsing 2: the regexing

--- src/parse.jl
+++ src/parse.jl
@@ -12,6 +12,16 @@ const STRING_TO_PERIOD = Dict(
     "Nanosecond" => Nanosecond,
 )
 
+# Regex components for our interval parsing
+const LEFT_INC_RE = r"^(\[|\()"
+const RIGHT_INC_RE = r"(\]|\))$"
+const DATE_RE = r"(\d{4})-(\d{1,2})-(\d{1,2})"
+const HR_RE = r" (H[EB])(\d{1,2})"
+const TZ_RE = r"([+-][\d|:]+)"
+
+# Working on incorporating this into base
+_regex(re::Regex...) = Regex(string([r.pattern for r in re]...))
+
 function Base.parse(::Type{Period}, str::AbstractString)
     m = match(r"^(?<type>\w+)\((?<value>-?\d+)\)$", str)
 
@@ -31,98 +41,86 @@ end
 #=
 Code for parsing intervals with a focus on testing DateTimes and ZonedDateTimes
 
-In order to guarantee that datetime parsing will work properly you'll need to call
+While we can handle the milliseconds issue with ZonedDateTimes, you should call
 ```
 Dates.format(dt, Dates.ISODateTimeFormat)
 ```
-and
+or
 ```
 Dates.format(zdt, TimeZones.ISOZonedDateTimeFormat)
 ```
-when generating the strings.
-
-NOTE: We can currently handle when milliseconds are missing.
+to ensure that you're output strings are parsable.
 =#
 
 function Base.parse(::Type{Interval{T}}, str::AbstractString) where T
     #=
-    print(
-        io,
-        first(inclusivity(interval)) ? "[" : "(",
-        first(interval),
-        " .. ",
-        last(interval),
-        last(inclusivity(interval)) ? "]" : ")",
-    )
+    Example: "(2018-12-02T23:00:00-06:00 .. 2018-12-03T00:00:00-06:00]"
     =#
-    a, b = parse_inclusivity(str)
-    f, l = _parse.(T, strip.(split(str[2:end-1], "..")))
+    re = _regex(LEFT_INC_RE, r"([\d\-T:.]+) .. ([\d\-T:.]+)", RIGHT_INC_RE)
+    m = match(re, str)
 
-    return Interval(f, l, a, b)
+    if m === nothing
+        throw(ArgumentError("Failed to parse Interval string $str w/ $re"))
+    end
+
+    a, f, l, b = m.captures
+    return Interval(_parse(T, f), _parse(T, l), a == "[", b == "]")
 end
 
-function Base.parse(::Type{<:AnchoredInterval{P, <:AbstractDateTime}}, str::AbstractString) where P
-    # (2018-12-02 HE24-06:00]
-    a, b = parse_inclusivity(str)
-    dt, suffix = split(str[2:end-1], ' ')
+function Base.parse(
+    I::Type{<:Union{HourBeginning{T}, HourEnding{T}}}, str::AbstractString
+) where T <: DateTime
+    #=
+    Example: "[2019-01-18 HB16)"
+    =#
+    re = _regex(LEFT_INC_RE, DATE_RE, HR_RE, r".*", RIGHT_INC_RE)
+    a, b, f, dt, m = _extract(I, re, str)
 
-    f = if startswith(suffix, "HE")
-        HE
-    elseif startswith(suffix, "HB")
-        HB
-    else
-        throw(ArgumentError("Can only parse HE and HB anchored intervals"))
-    end
+    return f(DateTime(dt...), a, b)
+end
 
-    hr = parse(Int, suffix[3:4])
-
-    anchor = if length(suffix) > 4
-        ZonedDateTime(parse.(Int, split(dt, '-'))..., hr, FixedTimeZone(suffix[5:end]))
-    else
-        DateTime(parse.(Int, split(dt, '-'))..., hr)
-    end
-
-    return f(anchor, a, b)
+function Base.parse(
+    I::Type{<:Union{HourBeginning{T}, HourEnding{T}}}, str::AbstractString
+) where T <: ZonedDateTime
+    #=
+    Example: "[2019-01-18 HB16-06:00)"
+    =#
+    re = _regex(LEFT_INC_RE, DATE_RE, HR_RE, TZ_RE, RIGHT_INC_RE)
+    a, b, f, dt, m = _extract(I, re, str)
+    return f(ZonedDateTime(dt..., FixedTimeZone(m[7])), a, b)
 end
 
 _parse(::Type{T}, str::AbstractString) where T = parse(T, str)
 
-function _parse(::Type{T}, str::AbstractString) where T <: DateTime
-    if '.' in str
-        parse(T, str)
-    else
-        parse(T, "$str.0")
-    end
-end
-
 function _parse(::Type{T}, str::AbstractString) where T <: ZonedDateTime
-    if '.' in str
-        parse(T, str)
-    else
-        dt, t = split(str, 'T')
-        tz_char = '+' in t ? '+' : '-'
-        s = string(dt, 'T', join(split(t, tz_char), ".000$tz_char"))
-        # println(s)
-        parse(T, s)
+    #=
+    Handle splitting up and inserting the milliseconds when they aren't in the string.
+
+    Example: "2019-01-18T17:00:00-06:00" -> parse(ZonedDateTime, "2019-01-18T17:00:00.000-06:00")
+    =#
+    re = _regex(r"^([\d\-]+T[\d:]+)(.\d{1,3})?([+-][[\d:]+)$")
+    m = match(re, str)
+
+    if m == nothing
+        throw(ArgumentError("Failed to parse ZonedDateTime string $str w/ $re"))
     end
+
+    ms = m[2] === nothing ? ".000" : m[2]
+    s = string(m[1], ms, m[3])
+    parse(T, s)
 end
 
-function parse_inclusivity(str::AbstractString)
-    a = if str[1] == '['
-        true
-    elseif str[1] == '('
-        false
-    else
-        throw(ArgumentError("Interval strings must start with '[' or '('."))
+function _extract(::Type{<:AnchoredInterval}, re::Regex, str::AbstractString)
+    m = match(re, str)
+
+    if m === nothing
+        throw(ArgumentError("Failed to parse AnchoredInterval string $str w/ $re"))
     end
 
-    b = if str[end] == ']'
-        true
-    elseif str[end] == ')'
-        false
-    else
-        throw(ArgumentError("Interval strings must end with ']' or ')'."))
-    end
+    a = m[1] == "["
+    b = m.captures[end] == "]"
+    f = m[5] == "HE" ? HE : HB
+    dt = parse.(Int, m.captures[[2, 3, 4, 6]])
 
-    return a, b
+    return a, b, f, dt, m
 end
