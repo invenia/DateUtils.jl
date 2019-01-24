c11ba1363913c6f0078d2ee3137904c13bdadb90
rofinn <rory.finnegan@gmail.com>
2019-01-24 15:12:02 -0600

    Drop single letter variables in tests.

--- test/parse.jl
+++ test/parse.jl
@@ -14,17 +14,17 @@ using TimeZones
 end
 
 @testset "parse datetimes" begin
-    a = DateTime(2019, 1, 18, 16)
-    b = DateTime(2019, 1, 18, 16, 46, 12, 285)
-    x = ZonedDateTime(a, tz"America/Winnipeg")
-    y = ZonedDateTime(b, tz"America/Winnipeg")
+    dt_a = DateTime(2019, 1, 18, 16)
+    dt_b = DateTime(2019, 1, 18, 16, 46, 12, 285)
+    zdt_a = ZonedDateTime(dt_a, tz"America/Winnipeg")
+    zdt_b = ZonedDateTime(dt_b, tz"America/Winnipeg")
 
     @testset "_parse DateTime" begin
         # Test that date formating and stringification hasn't changed
-        @test "2019-01-18T16:00:00.0" == Dates.format(a, Dates.ISODateTimeFormat)
-        @test "2019-01-18T16:46:12.285" == Dates.format(b, Dates.ISODateTimeFormat)
-        @test "2019-01-18T16:00:00" == string(a)
-        @test "2019-01-18T16:46:12.285" == string(b)
+        @test Dates.format(dt_a, Dates.ISODateTimeFormat) == "2019-01-18T16:00:00.0"
+        @test Dates.format(dt_b, Dates.ISODateTimeFormat) == "2019-01-18T16:46:12.285"
+        @test string(dt_a) == "2019-01-18T16:00:00"
+        @test string(dt_b) == "2019-01-18T16:46:12.285"
 
         # Test that _parse works like normal datetime parsing
         @test DateUtils._parse(DateTime, "2019-01-18T16:00:00.0") == parse(DateTime, "2019-01-18T16:00:00.0")
@@ -35,10 +35,10 @@ end
     end
 
     @testset "_parse ZonedDateTime" begin
-        @test "2019-01-18T16:00:00.000-06:00" == Dates.format(x, TimeZones.ISOZonedDateTimeFormat)
-        @test "2019-01-18T16:46:12.285-06:00" == Dates.format(y, TimeZones.ISOZonedDateTimeFormat)
-        @test "2019-01-18T16:46:12.285-06:00" == string(y)
-        @test "2019-01-18T16:00:00-06:00" == string(x)
+        @test Dates.format(zdt_a, TimeZones.ISOZonedDateTimeFormat) == "2019-01-18T16:00:00.000-06:00"
+        @test Dates.format(zdt_b, TimeZones.ISOZonedDateTimeFormat) == "2019-01-18T16:46:12.285-06:00"
+        @test string(zdt_b) == "2019-01-18T16:46:12.285-06:00"
+        @test string(zdt_a) == "2019-01-18T16:00:00-06:00"
 
         @test DateUtils._parse(ZonedDateTime, "2019-01-18T16:00:00.000-06:00") == parse(ZonedDateTime, "2019-01-18T16:00:00.000-06:00")
         @test DateUtils._parse(ZonedDateTime, "2019-01-18T16:46:12.285-06:00") == parse(ZonedDateTime, "2019-01-18T16:46:12.285-06:00")
@@ -48,14 +48,14 @@ end
         @test_throws ArgumentError parse(ZonedDateTime, "2019-01-18T16:00:00-06:00")
     end
 
-    anchored_intervals = [HE.((a, b, x, y))..., HB.((a, b, x, y))...]
+    anchored_intervals = [HE.((dt_a, dt_b, zdt_a, zdt_b))..., HB.((dt_a, dt_b, zdt_a, zdt_b))...]
     @testset "parse AnchoredInterval" begin
         for i in anchored_intervals
             @test parse(typeof(i), string(i)) == i
         end
     end
 
-    intervals = [Interval(x, x + Hour(1)) for x in (a, b, x, y)]
+    intervals = [Interval(x, x + Hour(1)) for x in (dt_a, dt_b, zdt_a, zdt_b)]
     @testset "parse Interval" begin
         for i in intervals
             @test parse(typeof(i), string(i)) == i
